03383bb29d6d6c542b1e716d24c4c044a1ef8552
alan vergara <alan_vergara_p@hotmail.com>
2022-10-07 10:36:05 -0500

    add seeds for restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+CATEGORY =  %w[chinese italian japanese french belgian]
+puts 'creating 5 restaurants'
+puts '...'
+5.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone, category: CATEGORY.sample)
+  Review.create(content: Faker::Restaurant.review, rating: rand(0..5), restaurant_id: restaurant.id)
+end
+puts 'done!!!'
